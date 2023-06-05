9473d5bb535dfc61471c45104f83d3a2ec9a2e52
Pawandeep Singh <pawandeepss@u.nus.edu>
2023-06-05 17:09:21 +0200

    Initial Commit

---
+++ MindChatr/MindChatrApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindChatrApp.swift
+//  MindChatr
+//
+//  Created by Pawandeep Sekhon on 5/6/23.
+//
+
+import SwiftUI
+
+@main
+struct MindChatrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
